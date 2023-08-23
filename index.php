47fca1887bfdf029f9e5258aaaf00bb04ce2e5fa
AgungAriGunayasa <121653737+AgungAriGunayasa@users.noreply.github.com>
2023-08-23 11:50:31 +0800

    Membuat files index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Agung Ari Gunayasa">
+    <title>Latihan GIT</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="position-absolute top-50 start-50 translate-middle">
+            <div class="shadow p-3 mb-5 bg-body-tertiary rounded text-center">
+                <h2>Biodata</h2><hr>
+                Nama : Anak Agung Ngurah Agung Ari Gunayasa<br>
+                Asal Perguruan Tinggi : ITB STIKOM Bali<br>
+                Jurusan : Sistem Informasi<br>
+                Activity : Full Stack Web Developer
+            </div>
+    </div>
+</body>
+</html>
